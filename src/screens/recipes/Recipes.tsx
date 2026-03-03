4d9e106a029c3c8c328dac3fde8a798edaba99b5
Jacob <66188949+AttemptFifteen@users.noreply.github.com>
2026-03-03 12:41:25 -0600

    Scroll view with text
    Implemented a scroll view with basic text and placeholder images.

--- src/screens/recipes/Recipes.tsx
+++ src/screens/recipes/Recipes.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 export function Recipes() {
   const heroImage = useState("../../assets/images/placeholders/rice.png");
@@ -11,9 +11,62 @@ export function Recipes() {
             require("../../../assets/images/placeholders/rice.png")
           }
         />
-      <Text>
-        Hello there.
-      </Text>
+      <ScrollView style={styles.scrollView} contentContainerStyle={{ paddingBottom: 100 }}>
+        <View style={styles.card}>
+          <Text style={styles.title}>
+            Chicken Fried Rice
+          </Text>
+          <Text style={styles.recipeType}>
+            Lunch / Dinner
+          </Text>
+          <Text style={styles.bodyText}>
+            Hello there!
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <View style={styles.horizontalFlex}>
+            <View style={styles.detailCard}>
+              <Text style={styles.infoText}>Calories</Text>
+              <Text style={styles.bold}>350</Text>
+            </View>
+            <View style={styles.detailCard}>
+              <Text style={styles.infoText}>Carbs</Text>
+              <Text style={styles.bold}>350</Text>
+            </View>
+            <View style={styles.detailCard}>
+              <Text style={styles.infoText}>Fats</Text>
+              <Text style={styles.bold}>350</Text>
+            </View>
+            <View style={styles.detailCard}>
+              <Text style={styles.infoText}>Protein</Text>
+              <Text style={styles.bold}>350</Text>
+            </View>
+          </View>
+          <Text style={styles.header}>
+            Ingredients
+          </Text>
+          <Image
+            style={styles.recipeImage}
+            source={
+                require("../../../assets/images/placeholders/rice.png")
+              }
+          />
+          <Text style={styles.bodyText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -21,11 +74,63 @@ export function Recipes() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#FFFFFF"
+  },
+  card: {
+    backgroundColor: "white",
+    marginTop: 300,
+    borderRadius: 20,
+    padding: 20,
+    boxShadow: "0px 0px 10px #101010"
+  },
+  detailCard: {
+    backgroundColor: "#F5F5F5",
+    width: 70,
+    height: 70,
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 20,
+    margin: 10
   },
   heroImage: {
-    height: 200
+    height: 300,
+    width: "100%",
+    position: "absolute",
+    top: 50
+  },
+  scrollView: {
+    position: "relative",
+  },
+  title: {
+    fontSize: 30,
+  },
+  recipeType: {
+    fontSize: 20,
+    color: "#00000080"
+  },
+  bodyText: {
+    fontSize: 20,
+    marginTop: 20
+  },
+  bold: {
+    fontWeight: "bold"
+  },
+  infoText: {
+    color: "#00000080"
+
+  },
+  header: {
+    fontSize: 25,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  recipeImage: {
+    position: "relative",
+    width: "100%",
+    maxHeight: 600
+  },
+  horizontalFlex: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   }
 });
