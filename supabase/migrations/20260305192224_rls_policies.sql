95f431faae435d9f209dfd84f06a3e039bc2ce58
Ashton Hull <ashton.hull2022@gmail.com>
2026-03-10 17:07:52 -0500

    fixed performance issue on RLS rules

--- supabase/migrations/20260305192224_rls_policies.sql
+++ supabase/migrations/20260305192224_rls_policies.sql
@@ -9,7 +9,7 @@ POLICY "Users can access own pantry"
 ON pantries
 FOR ALL
 USING( pantry_id IN (
-        SELECT pantry_id FROM users WHERE user_id = auth.uid()
+        SELECT pantry_id FROM users WHERE user_id = (SELECT auth.uid())
     )
 );
 
@@ -17,14 +17,14 @@ CREATE
 POLICY "Users can access own user details"
 ON users
 FOR ALL
-USING(user_id = auth.uid());
+USING(user_id = (SELECT auth.uid()));
 
 CREATE
 POLICY "Users can access own pantry ingredients"
 ON pantry_ingredients
 FOR ALL
     USING (pantry_id IN (
-        SELECT pantry_id FROM users WHERE user_id = auth.uid()
+        SELECT pantry_id FROM users WHERE user_id = (SELECT auth.uid())
     )
 );
 
@@ -32,10 +32,10 @@ CREATE
 POLICY "Users can access own preferences"
 ON user_preferences
 FOR ALL
-USING(user_id = auth.uid());
+USING(user_id = (SELECT auth.uid()));
 
 CREATE
 POLICY "Users can access own recipe details"
 ON recipes
 FOR ALL
-USING(user_id = auth.uid());
+USING(user_id = (SELECT auth.uid()));
